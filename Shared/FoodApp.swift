c9eda7224d31d4e0ba2763a3fa74206750a75fe5
Hugh Bromund <hughbromund44@gmail.com>
2020-07-05 17:42:30 -0400

    Initial Commit

---
+++ Shared/FoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodApp.swift
+//  Shared
+//
+//  Created by Hugh Bromund on 7/5/20.
+//
+
+import SwiftUI
+
+@main
+struct FoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
